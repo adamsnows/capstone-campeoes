1cb2acab98e7da7d7e4486b5755dbe5adab3ee58
Artorys <darksoulsgui123@gmail.com>
2022-07-11 16:56:47 -0300

    initial config

---
+++ script.js
@@ -0,0 +1,23 @@
+//Models
+import {UserLogin} from "./src/models/Login/UserLogin.js"
+import {UserHabitsAll} from "./src/models/Habits/UserHabitsAll.js"
+import {UserHabitTitle} from "./src/models/Habits/UserHabitTitle.js"
+import {UserHabitDescription} from "./src/models/Habits/UserHabitDescription.js"
+import {UserHabitCategory} from "./src/models/Habits/UserHabitCategory.js"
+import {UserHabitStatus} from "./src/models/Habits/UserHabitStatus.js"
+import {userProfileAll} from "./src/models/Profile/UserProfileAll.js"
+import {userProfileImg} from "./src/models/Profile/UserProfileImg.js"
+import {userProfileName} from "./src/models/Profile/UserProfileName.js"
+//views
+import {CreateHabits} from "./src/views/CreateHabits.js"
+import {EditHabits} from "./src/views/EditHabits.js"
+import {DeleteHabits} from "./src/views/DeleteHabits.js"
+import {EditProfile} from "./src/views/EditProfile.js"
+import {Home} from "./src/views/Home.js"
+//Controllers
+import {ApiController} from "./src/controller/Api.controller.js"
+import {LoginController} from "./src/controller/Status.controller.js"
+import {EditController} from "./src/controller/Edit.controller.js"
+import {DeleteController} from "./src/controller/Delete.controller.js"
+import {StatusController } from "./src/controller/Status.controller.js"
+
---
+++ src/models/Habits/UserHabitDescription.js
@@ -0,0 +1,7 @@
+class UserHabitDescription
+{
+    constructor(habit_description)
+    {
+        this.habit_description = habit_description
+    }
+}
